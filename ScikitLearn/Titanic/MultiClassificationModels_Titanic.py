0f91f4eef970b18950efb4ee9b6258d27a847527
Sarthak Sahu <32789001+Sarthak99@users.noreply.github.com>
2019-07-22 12:35:17 +0530

    Update MultiClassificationModels_Titanic.py

--- ScikitLearn/Titanic/MultiClassificationModels_Titanic.py
+++ ScikitLearn/Titanic/MultiClassificationModels_Titanic.py
@@ -22,3 +22,47 @@ titanic_df = pd.read_csv("D:\\Git\\MachineLearning\\ScikitLearn\\Titanic\\datase
 features = list(titanic_df.columns[1:])
 
 result_dict={}
+
+#Summarizing output
+def summarize_classification(y_test, y_pred):
+    
+    acc = accuracy_score(y_test, y_pred, normalize=True)
+    num_acc =accuracy_score(y_test, y_pred, normalize=False)
+    
+    prec = precision_score(y_test, y_pred)
+    recall = recall_score(y_test,y_pred)
+    
+    return ("accuracy" : acc,
+            "precission" : prec,
+            "recall" : recall,
+            "accuracy_count" : numm_acc)
+
+#Helper function for building models
+def build_model(classifier_fn,
+                name_of_x_col,
+                name_of_y_col,
+                dataset,
+                test_frac=0.2):
+    
+    X = dataset[name_of_x_col]
+    Y = dataset[name_of_y_col]
+    
+    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=test_frac)
+    
+    model = classifier_fn(x_train, y_train)
+    
+    y_pred = model.predict(y_test)
+    
+    y_pred_train = model.predict(y_train)
+    
+    train_summary = summarize_classification(y_train, y_pred_train)
+    test_summary = summarize_classification(y_test, y_pred)
+    
+    pred_result = pd.DataFrame({"y_test":y_test,
+                                "y_pred":y_pred})
+    
+    model.crosstab = pd.crosstab(pred_result.y_pred, pred_result.y_test)
+    
+    return {"training": train_summary,
+            "test": test_summary,
+            "confusion_matrix": model.crosstab}
